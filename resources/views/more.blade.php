d5324b92d55084ab5d72b67ffe877aa07d75ea24
GiulioDangelo <dangelogiulio48@gmail.com>
2023-06-22 15:30:25 +0200

    push

---
+++ resources/views/more.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.nav{
+    background-color: black;
+    height: 70px;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+a{
+    text-decoration: none;
+    color: white;
+    font-size: 50px;
+}
+</style>
+<body>
+    <div class="nav">
+        <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/more">more</a></li>
+        </ul>
+    </div>
+
+    <h1>more page</h1>
+</body>
+</html>
